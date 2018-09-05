837c272cc8b3fa1596caa74f529fd4d64f22e5a8
Percy Maldonado Quispe <percy.maldonado@ucsp.edu.pe>
2018-09-05 10:27:55 -0500

    opengl select

--- graph.cpp
+++ graph.cpp
@@ -3,14 +3,18 @@
 #include <GL/glut.h>
 
 #define KEY_ESC 27
-#define KEY_E 'e'
-#define KEY_V 'v'
-#define KEY_R 'r'
+#define KEY_E 'e'       // show edge
+#define KEY_V 'v'       // show vertex
+#define KEY_R 'r'       // remove into 2 point
+#define KEY_S 's'       // get node source
+#define KEY_T 't'       // get node target
+#define KEY_A 'a'
 
 #include "src/graph.h"
 
 typedef traits<float, double, double>	box;	// type box
 typedef cpoint<box>						point;	// point
+typedef cnode<box>                      node;  // point
 typedef cgraph<box>						graph;	// graph
 
 graph *tmp;								// graph
@@ -27,13 +31,19 @@ point pmax(col_size+ts,row_size+ts);	// max point
 point minr(0,0);
 point maxr(0,0);
 
+node *node_source = new node(point(0,0));
+node *node_target = new node(point(0,0));
+
 bool state_edge = true;
 bool state_vertex = true;
 bool state_resize = true;
 
+bool state_source = false;
+bool state_target = false;
+
 void gldraw(){
 	if(state_vertex){
-	    glPointSize(3);
+	    glPointSize(2);
 	    glBegin(GL_POINTS);
 	        glColor3d(255,0,0);
 	        for(unsigned i=0; i<vpoints.size(); i++)
@@ -51,6 +61,24 @@ void gldraw(){
 			}		
 		glEnd();
 	}
+
+    if(state_source){
+        glPointSize(5);
+        glBegin(GL_POINTS);
+            glColor3d(255,255,255);
+            glVertex2d(node_source->m_data.get_x(),
+                node_source->m_data.get_y());
+        glEnd();
+    }
+
+    if(state_target){
+        glPointSize(5);
+        glBegin(GL_POINTS);
+            glColor3d(255,255,0);
+            glVertex2d(node_target->m_data.get_x(),
+                node_target->m_data.get_y());
+        glEnd();
+    }
 }
 
 void glpaint(){
@@ -111,6 +139,27 @@ GLvoid window_key(unsigned char key, int x, int y) {
         	tmp->remove_node_in(minr, maxr);
         	// std::cout << minr << "\t" << maxr << "\n";
         	break;
+        case KEY_S:{
+            if(!state_resize){
+                state_resize = true;
+                node_source = tmp->near_to(minr);
+                std::cout << node_source->m_data << "\n";
+                state_source = true;
+            }            
+            break;
+        }
+        case KEY_T:{
+            if(!state_resize){
+                state_resize = true;
+                node_target = tmp->near_to(minr);
+                std::cout << node_target->m_data << "\n";
+                state_target = true;
+            }            
+            break;
+        }
+        case KEY_A:
+            tmp->a_asterisk(node_source, node_target);
+            break;
         default:
             break;
     }
@@ -123,8 +172,6 @@ int main(int argc, char *argv[]){
 
 	std::cout << "min: " << pmin << "\n";
 	std::cout << "max: " << pmax << "\n";
-
-	tmp->search(point(250,250), point(800,450), 6);
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
