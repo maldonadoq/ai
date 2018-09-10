1ee65032e40a5637ed6f19bd577f73c2807bc8e1
percy0001 <percy.maldonado@ucsp.edu.pe>
2018-09-09 20:01:26 -0500

    search's algorithm

--- src/graph.h
+++ src/graph.h
@@ -5,12 +5,25 @@
 #include <utility>
 #include <vector>
 #include <list>
-
-static std::vector<std::pair<double, double> > vpoints;
-static std::vector<std::pair<double, double> > vlines;
+#include <stack>
+#include <queue>
 
 #include "point.h"
 #include "node.h"
+
+extern std::vector<std::pair<double, double> > vpoints;
+extern std::vector<std::pair<double, double> > vlines;
+static std::vector<std::pair<double, double> > vpatha;
+static std::vector<std::pair<double, double> > vpathd;
+static std::vector<std::pair<double, double> > vpathb;
+
+template<class T>
+class compare{
+public:
+    bool operator()(T A, T B){
+        return A->get_hg() > B->get_hg();
+    }
+};
 
 // regular matrix
 template<class G>
@@ -19,6 +32,7 @@ private:
 	typedef typename G::node	node;	// class node
     typedef typename G::point	point;	// class point
     typedef typename G::W 		W;
+    typedef typename G::H 		H;
 
 	unsigned m_srow, m_scol;	// size in pixel of row and col
 	unsigned m_nrow, m_ncol;	// number of row and col of regular matrix
@@ -40,9 +54,10 @@ public:
 	cgraph(){}
 
 	void create_regular_vertex(){
-		for(unsigned i=0; i<this->m_nrow; i++){
-			for(unsigned j=0; j<this->m_ncol; j++){
-				this->m_vnodes[i][j] = new node(point(this->m_unit*j, this->m_unit*i));
+		unsigned i,j,k=0;
+		for(i=0; i<this->m_nrow; i++){
+			for(j=0; j<this->m_ncol; j++){
+				this->m_vnodes[i][j] = new node(point(this->m_unit*j, this->m_unit*i), k++);
 			}
 		}
 	}
@@ -129,24 +144,152 @@ public:
 		return NULL;
 	}
 
-	void restart_target(node *t){
+	void restart_target(){
 		for(unsigned i=0; i<this->m_nrow; i++){
 			for(unsigned j=0; j<this->m_ncol; j++){
 				if(this->m_vnodes[i][j] != NULL){
-					this->m_vnodes[i][j]->set_heuristic(t);
 					this->m_vnodes[i][j]->m_state = false;
 				}
 			}
 		}
 	}
 
-	void a_asterisk(node *s, node *t){
-		typename std::list<node *>::iterator it;
-		for(it=s->m_lady.begin(); it!=s->m_lady.end(); it++){
-			std::cout << (*it)->m_data << "\t";
+	void restart_path(std::vector<std::pair<double, double> > &pt, point **cf, point src, point tar){				
+		pt.clear();
+		
+		bool t = true;
+		unsigned tx,ty;
+		point current = tar;
+
+		pt.push_back(std::make_pair(current.get_x(),current.get_y())); 
+
+		while(current != src){
+			tx = current.get_x()/m_unit;
+			ty = current.get_y()/m_unit;
+
+			current = cf[ty][tx];
+			pt.push_back(std::make_pair(current.get_x(),current.get_y())); 
+			//std::cout << current << "\t";
 		}
-		std::cout << "\n";
-	}	
+	}
+
+	void a_asterisk(node *s, node *t){
+		restart_target();		
+
+		typename std::list<node *>::iterator it;
+		std::priority_queue<node *, std::vector<node *>, compare<node *> > pq;		
+
+		pq.push(s);
+		node* current;
+
+		H h;
+		W g;
+
+		unsigned tx,ty, i;
+		point **camefrom = new point*[m_nrow];		
+		for(i=0; i<m_nrow; i++)	camefrom[i] = new point[m_ncol];
+
+		while(!pq.empty()){
+			current = pq.top();
+			pq.pop();
+		
+			if(current == t){
+				restart_path(vpatha, camefrom, s->m_data, t->m_data);
+				return;
+			}
+
+			for(it=current->m_lady.begin(); it!=current->m_lady.end(); it++){
+				if(!(*it)->m_state){
+					(*it)->m_state = true;
+					h = (*it)->distance(t);
+					g = current->distance(*it);
+					(*it)->set_hg(h+g);
+					pq.push(*it);
+
+
+					tx = (*it)->m_data.get_x()/m_unit;
+					ty = (*it)->m_data.get_y()/m_unit;
+
+					camefrom[ty][tx] = point(current->m_data.get_x(), current->m_data.get_y());
+				}
+			}
+		}
+
+		delete []camefrom;
+	}
+
+	void depth_search(node* s, node* t){
+		restart_target();
+
+		std::stack<node *> v;
+		v.push(s);
+		
+		node *current;
+		typename std::list<node* >::iterator it;
+
+		unsigned tx,ty, i;
+		point **camefrom = new point*[m_nrow];		
+		for(i=0; i<m_nrow; i++)	camefrom[i] = new point[m_ncol];
+
+		while(!v.empty()){
+			current = v.top();				// use 		
+			v.pop();
+			
+			if(current == t){
+				restart_path(vpathd, camefrom, s->m_data, t->m_data);
+				return;
+			}
+
+			for(it=current->m_lady.begin(); it!=current->m_lady.end(); it++){
+				if(!(*it)->m_state){
+					(*it)->m_state = true;		// visited
+					v.push(*it);
+
+					tx = (*it)->m_data.get_x()/m_unit;
+					ty = (*it)->m_data.get_y()/m_unit;
+
+					camefrom[ty][tx] = point(current->m_data.get_x(), current->m_data.get_y());
+				}
+			}
+		}
+	}
+
+
+	void breadth_search(node* s, node* t){
+		restart_target();
+
+		std::queue<node *> v;
+		v.push(s);
+
+		node *current;
+		typename std::list<node* >::iterator it;
+
+		unsigned tx,ty, i;
+		point **camefrom = new point*[m_nrow];		
+		for(i=0; i<m_nrow; i++)	camefrom[i] = new point[m_ncol];
+
+		while(!v.empty()){
+			current = v.front();
+			
+			if(current == t){
+				restart_path(vpathb, camefrom, s->m_data, t->m_data);
+				return;
+			}
+
+			for(it=current->m_lady.begin(); it!=current->m_lady.end(); it++){
+				if(!(*it)->m_state){
+					(*it)->m_state = true;		// visited
+					v.push(*it);
+
+					tx = (*it)->m_data.get_x()/m_unit;
+					ty = (*it)->m_data.get_y()/m_unit;
+
+					camefrom[ty][tx] = point(current->m_data.get_x(), current->m_data.get_y());
+				}
+			}
+			v.pop();
+		}
+	}
 
 	node* near_to(point a){
 		node* tmp = NULL;
